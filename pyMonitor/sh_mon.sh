875aafa190d9b38d5a867f070d8e53d72adb860c
Rahul Sharma <rsconceptx@gmail.com>
2015-09-13 13:51:13 +0530

    standalone bash script to print bytes recvd

--- pyMonitor/sh_mon.sh
+++ pyMonitor/sh_mon.sh
@@ -1,2 +1,3 @@
 IN_MB=(1024*1024)
-cat /proc/net/dev | grep "wlan" | awk '{print $2/IN_MB}' 
+INFO=cat /proc/net/dev | grep "wlan" | awk '{print $2/IN_MB}'
+notify-send -h int:y:10 -u normal -t 10000 "Total Bytes Received ${INFO}"
