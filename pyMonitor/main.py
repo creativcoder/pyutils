b6ee9b6f1a329d272023b9968ca7faceb9f01644
Rahul Sharma <rsconceptx@gmail.com>
2015-09-14 23:47:20 +0530

    Update main.py

--- pyMonitor/main.py
+++ pyMonitor/main.py
@@ -3,4 +3,6 @@ import subprocess
 
 BYTES_RECV = psutil.net_io_counters(pernic=True).get('wlan0').bytes_recv/float(1024)/1024
 
-subprocess.Popen(['bash','./notify.sh',str(BYTES_RECV])
+#cmd = """sudo cat /proc/net/dev | grep "wlan" | awk '{print $2}'"""
+
+a=subprocess.Popen(['bash','./notify.sh',str(round(BYTES_RECV,2))])
