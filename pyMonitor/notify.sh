360b8605157686c310b233928051760f88da84b4
Rahul Sharma <rsconceptx@gmail.com>
2015-09-14 23:48:21 +0530

    Update notify.sh

--- pyMonitor/notify.sh
+++ pyMonitor/notify.sh
@@ -1,2 +1,1 @@
-# cat /proc/net/dev | grep "wlan" | awk '{print $2}'
-notify-send -h int:y:10 -u normal -t 100 "Total Bytes Received $1 MB"
+notify-send -h int:y:10 -u normal -t 100 "Current Data Usage $1 MB"
