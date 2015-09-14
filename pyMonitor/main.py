23bf9236b295b3515086a8674200861d59f80b85
Rahul Sharma <rsconceptx@gmail.com>
2015-09-14 23:40:03 +0530

    Update main.py

--- pyMonitor/main.py
+++ pyMonitor/main.py
@@ -1,6 +1,6 @@
 import psutil
 import subprocess
 
-BYTES_RECV = psutil.net_io_counters().bytes_recv
+BYTES_RECV = psutil.net_io_counters(pernic=True).get('wlan0').bytes_recv/float(1024)/1024
 
-subprocess.Popen(['bash','./notify.sh',str(BYTES_RECV/1048576)])
+subprocess.Popen(['bash','./notify.sh',str(BYTES_RECV])
