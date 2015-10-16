ab14e1e1716c652083db2a431b3e56e64e44a340
Rahul Sharma <rsconceptx@gmail.com>
2015-10-16 06:46:08 +0530

    added droid bootstrap

---
+++ droid_bootstrap.sh
@@ -0,0 +1,5 @@
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+sudo apt-get install libc6-i386 lib32stdc++6 lib32gcc1 lib32ncurses5 lib32z1
+# ADB Path
+export PATH=$PATH:/home/<user>/Android/Sdk/platform-tools
